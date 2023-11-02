29b13a091591c6548de6ace31b5734809cb8d956
Moro Bamber <99629944+UnderYourSpell@users.noreply.github.com>
2023-11-02 14:54:10 -0800

    added some printing capabilities

--- as_info.py
+++ as_info.py
@@ -17,7 +17,7 @@ class ASInfo():
         #asn_peers = as_api.peers(asn)
         num_peers_v4 = len(asn_peers['data']['ipv4_peers'])
 
-        print("\nipv4 peers: ")
+        print(f"\n{num_peers_v4} ipv4 peers: ")
         for peer in range(num_peers_v4):
             isp = asn_peers['data']['ipv4_peers'][peer]['name']
             isp_asnum = asn_peers['data']['ipv4_peers'][peer]['asn']
@@ -27,8 +27,46 @@ class ASInfo():
             #could maybe make this optional
             num_peers_v6 = len(asn_peers['data']['ipv6_peers'])
 
-            print("\nipv6 peers: ")
+            print(f"\n{num_peers_v6} ipv6 peers: ")
             for peer in range(num_peers_v6):
                 isp = asn_peers['data']['ipv6_peers'][peer]['name']
                 isp_asnum = asn_peers['data']['ipv6_peers'][peer]['asn']
                 print(str(isp) + ' AS#: ' + str(isp_asnum))
+
+    def print_ixs(self):
+        asn_ixs = self.ixs
+        ixs = len(asn_ixs['data'])
+
+        print(f"\n{ixs} Internet Exchange Points: ")
+        for ix in range(ixs):
+            ix_name = asn_ixs['data'][ix]['name']
+            ix_id = asn_ixs['data'][ix]['ix_id']
+            print(str(ix_name) + ' with ix id: ' + str(ix_id))
+
+    def print_upstreams(self,v6 = False):
+        asn_upstreams = self.upstreams
+        len_up = len(asn_upstreams['data']['ipv4_upstreams'])
+
+        print(f"\n{len_up} IPv4 Upstream ASes: ")
+        for peer in range(len_up):
+            peer_name = asn_upstreams['data']['ipv4_supstreams'][peer]['name']
+            as_num = ['data']['ipv4_upstreams'][peer]['asn']
+            print(str(peer_name) + ' AS#: ' + str(as_num))
+        
+        if v6 == True:
+            len_up = len(asn_upstreams['data']['ipv6_upstreams'])
+
+            print(f"\n{len_up} IPv6 Upstream ASes: ")
+            for peer in range(len_up):
+                peer_name = asn_upstreams['data']['ipv6_supstreams'][peer]['name']
+                as_num = ['data']['ipv6_upstreams'][peer]['asn']
+                print(str(peer_name) + ' AS#: ' + str(as_num))
+
+
+    def retrieve_upstream_graph(self):
+        #api gives us a link to a graph
+        #this method will give this to us
+        pass
+
+    def print_downstreams(self):
+        pass
--- main.py
+++ main.py
@@ -5,15 +5,7 @@ from as_info import ASInfo
 def main():
     asn = ASInfo()
     asn.print_peers(v6 = False) #optional parameter True to also print v6 peers, default is False
-    
-    asn_ixs = asn.ixs
-    ixs = len(asn_ixs['data'])
-
-    print("\n\nAs at Internet Exchange Points: ")
-    for ix in range(ixs):
-        ix_name = asn_ixs['data'][ix]['name']
-        ix_id = asn_ixs['data'][ix]['ix_id']
-        print(str(ix_name) + ' with ix id: ' + str(ix_id))
-
+    asn.print_ixs()
+   
 if __name__ == "__main__":
     main()
